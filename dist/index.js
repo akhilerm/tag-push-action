d2004a20717fe40c4ea4a36e660b7e21bc27eef1
Akhil Mohan <akhil.mohan@mayadata.io>
2020-11-22 18:37:33 +0530

    change action and workflow
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- dist/index.js
+++ dist/index.js
@@ -42,6 +42,7 @@ function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const source = core.getInput('src');
+            // destination should be multiline . So multiple destinations can be specified
             const destination = core.getInput('dst');
             /*
             if source is empty {
@@ -53,7 +54,7 @@ function run() {
             return;
             }
             */
-            yield exec.exec('docker', ['run', '--rm', '-i', '-v /home/runner/.docker/config.json:/root/.docker/config.json', 'tonistiigi/repo-copy:latest', source, destination]);
+            yield exec.exec('docker', ['run', '--rm', '-i', '-v', '/home/runner/.docker/config.json:/root/.docker/config.json', 'tonistiigi/repo-copy:latest', source, destination]);
         }
         catch (error) {
             core.setFailed(error.message);
