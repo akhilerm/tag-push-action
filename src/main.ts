4f167844cf2db1658b608490efd301b9bc7fa226
Akhil Mohan <akhil.mohan@mayadata.io>
2021-02-16 11:12:15 +0530

    add nil check

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,55 @@
+import csvparse from 'csv-parse/lib/sync';
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
-//import csvparse from 'csv-parse/lib/sync';
 
 async function run(): Promise<void> {
   try {
     const source: string = core.getInput('src');
 
-    // TODO: users should be able to specify multiple destination. use getDestination()
-    // and use that
-    const destination: string = core.getInput('dst');
+    let destination: string[];
+    destination = await getDestinationTags();
 
-    await exec.exec('docker', ['run', '--rm', '-v', '/home/runner/.docker/config.json:/root/.docker/config.json', 'tonistiigi/repo-copy:latest', source, destination]);
+    if (source == '') {
+      core.setFailed('Source image not set');
+      return;
+    }
+
+    if (destination.length == 0) {
+      core.setFailed('Destination image not set');
+      return;
+    }
+
+	  
+    let dst: string = destination.join(' ');
+    
+    await exec.exec('docker', ['run', '--rm', '-i', '-v', '/home/runner/.docker/config.json:/root/.docker/config.json', 'tonistiigi/repo-copy:latest', source, dst]);
 
   } catch (error) {
     core.setFailed(error.message)
   }
 }
 
-/* get all the destination tags
-async function getDestination(): Promise<void> {
-  const dstList = core.getInput('dst');
+async function getDestinationTags(): Promise<string[]> {
+  let res: Array<string> = [];
 
-  for 
+  const items = core.getInput('dst');
+  if (items == '') {
+    return res;
+  }
+
+  for (let output of (await csvparse(items, {
+    columns: false,
+    relaxColumnCount: true,
+    skipLinesWithEmptyValues: true
+  })) as Array<string[]>) {
+    if (output.length == 1) {
+      res.push(output[0]);
+    } else {
+      res.push(...output);
+    }
+  }
+
+  return res.filter(item => item).map(pat => pat.trim());
 }
-*/
+
 run();
