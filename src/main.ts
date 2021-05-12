80e63be277b0f2532bd1766fc9a77fd383079477
Akhil Mohan <akhil.mohan@mayadata.io>
2021-05-12 17:24:34 +0530

    add tests
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function run(): Promise<void> {
 
 // This function is a modified version from the script used in docker buildx actions
 // Ref https://github.com/docker/build-push-action/blob/master/src/context.ts#L163
-async function getDestinationTags(): Promise<string[]> {
+export async function getDestinationTags(): Promise<string[]> {
   let res: Array<string> = [];
 
   const items = core.getInput('dst');
